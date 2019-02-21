0f0a34415c060c83f42abf0d559a5768d77774eb
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-21 20:16:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require "pry"
+
+katz_deli_queue = []
+
+def line(names)
+ if names == []
+   puts "The line is currently empty."
+ else
+   arr = Array.new
+   names.each_with_index do |name, index|
+   arr.push("#{index +1}. #{name}")
+   end
+    puts "The line is currently: #{arr.join(' ')}"
+  end
+end
+
+
+def take_a_number(arr, name)
+  array.katz_deli_queue.map do |arr, index |
+    #puts "Now serving #{name} number #{number + 1}"
+    puts "Welcome, #{arr}. You are number #{index} in line."
+  end
+end
+#binding.pry
+def now_serving
+  puts "There is nobody waiting to be served!"
+end
+
+
